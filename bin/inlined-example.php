10df3bc09c745f1979f2eed6e1fd000962deac08
Marco Pivetta <ocramius@gmail.com>
2017-05-28 19:40:08 +0200

    Adding example application - simple inlined version

--- bin/inlined-example.php
+++ bin/inlined-example.php
@@ -1,6 +1,27 @@
 #!/usr/bin/env php
 <?php
 
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$app = new Application(
+    'Example application - inlined version'
+);
 
+$app->add(new class extends Command {
+    protected function configure()
+    {
+        $this->setName('example:say-hello');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('<info>Hello World!</info>');
+    }
+});
+
+$app->run();
