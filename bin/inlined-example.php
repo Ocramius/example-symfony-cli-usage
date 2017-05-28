6e4763e88e3f42b764ffd349fbae7d46f5892e8d
Marco Pivetta <ocramius@gmail.com>
2017-05-28 19:42:37 +0200

    Adding a CLI command with a required argument

--- bin/inlined-example.php
+++ bin/inlined-example.php
@@ -3,14 +3,13 @@
 
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$app = new Application(
-    'Example application - inlined version'
-);
+$app = new Application('Example application - inlined version');
 
 $app->add(new class extends Command {
     protected function configure()
@@ -21,6 +20,22 @@ $app->add(new class extends Command {
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->writeln('<info>Hello World!</info>');
+    }
+});
+
+$app->add(new class extends Command {
+    protected function configure()
+    {
+        $this->setName('example:greet');
+        $this->addArgument('name', InputArgument::REQUIRED, 'Someone to greet');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln(sprintf(
+            '<info>Hello %s!</info>',
+            $input->getArgument('name')
+        ));
     }
 });
 
